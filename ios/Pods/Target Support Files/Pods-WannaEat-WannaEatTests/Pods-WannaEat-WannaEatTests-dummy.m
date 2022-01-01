b31c30fc9e4c506fcdbd5fbf1975a767659e0b2f
Jennfier Heejin Kang <39927897+HKang0302@users.noreply.github.com>
2022-01-01 23:28:02 +0900

    newly init for react-native

--- ios/Pods/Target Support Files/Pods-WannaEat-WannaEatTests/Pods-WannaEat-WannaEatTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WannaEat_WannaEatTests : NSObject
-@end
-@implementation PodsDummy_Pods_WannaEat_WannaEatTests
-@end
--- ios/Pods/Target Support Files/Pods-WannaEat/Pods-WannaEat-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WannaEat : NSObject
-@end
-@implementation PodsDummy_Pods_WannaEat
-@end
